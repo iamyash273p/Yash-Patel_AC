2d268fb02cd7f5b05dbef8e7ed5e35cc23548cfe
Yash Patel <yash273p@gmail.com>
2026-08-23 21:43:24 +0530

    LeapYear Completed and Assignment 2 Completed

--- Assignments/Logic Building/Day2/LeapYear.java
+++ Assignments/Logic Building/Day2/LeapYear.java
@@ -2,7 +2,13 @@ public class LeapYear {
     public static void main(String args[])
     {
         int year = 2016;
-
-        if(year )
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+        {
+            System.out.println("Leap Year");
+        }
+        else
+        {
+            System.out.println("Not a Leap Year");
+        }
     }
 }
