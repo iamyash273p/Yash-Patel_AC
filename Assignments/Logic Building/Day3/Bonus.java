49431cc4579b4ded44884a9d5a420c9ad44bb4d8
Yash Patel <yash273p@gmail.com>
2026-08-23 21:44:21 +0530

    Assignment 3 Completed

--- Assignments/Logic Building/Day3/Bonus.java
+++ Assignments/Logic Building/Day3/Bonus.java
@@ -23,7 +23,7 @@ class Choices
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
 
-        if(year % 4 == 0 && year % 4 != 0 || year % 400 == 0)
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
             System.out.println("Leap Year: " + year);
         else
             System.out.println("Not a LeapYear: " + year);
