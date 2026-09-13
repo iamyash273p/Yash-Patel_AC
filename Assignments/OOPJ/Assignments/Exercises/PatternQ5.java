a6e297978100102c1ba6b85b4dd02a14b559d037
Yash Patel <yash273p@gmail.com>
2026-09-14 00:37:50 +0530

    Add files via upload

---
+++ Assignments/OOPJ/Assignments/Exercises/PatternQ5.java
@@ -0,0 +1,15 @@
+
+public class PatternQ5 {
+
+	public static void main(String[] args) {
+		
+		for(int iTemp = 5; iTemp >= 1; iTemp--) {
+			for(int jTemp = iTemp; jTemp >= 1; jTemp--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Assignments/OOPJ/Assignments/Exercises/multipleOf7.java
@@ -0,0 +1,11 @@
+
+public class multipleOf7 {
+
+	public static void main(String[] args) {
+		
+		for(int iTemp = 7; iTemp < 200; iTemp += 7)
+			System.out.println(iTemp);
+		
+	}
+
+}
